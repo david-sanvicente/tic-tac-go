891cfc6f22274bb21dad454ec112aea28dfe3720
Keagan St. Rose <sanvicented1@gmail.com>
2022-09-27 15:58:02 -0400

    gradient background component for use in multiple screens

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,8 +1,9 @@
-import { View, Text, Image, Button } from 'react-native'
+import { View, Text, ScrollView, Image, Button } from 'react-native'
 import React, { ReactElement } from 'react'
 import styles from "./home.styles"
 import { StackNavigationProp } from "@react-navigation/stack"
 import { StackNavigatorParams } from "../../config/navigator"
+import { GradientBackground } from "../../components/"
 
 type HomeProps = {
   navigation: StackNavigationProp<StackNavigatorParams, "Home">
@@ -10,14 +11,16 @@ type HomeProps = {
 
 export default function Home({ navigation }: HomeProps): ReactElement {
   return (
-    <View style={styles.container}>
-      <Image source={require("../../../assets/favicon.png")} />
-      <Text>Home</Text>
-      <Button
-        title="Game"
-        onPress={() => {
-          navigation.navigate("Game", { gameId: '777' })
-        }} />
-    </View>
+    <GradientBackground >
+      <ScrollView contentContainerStyle={styles.container}>
+        <Image source={require("../../../assets/favicon.png")} />
+        <Text>Home</Text>
+        <Button
+          title="Game"
+          onPress={() => {
+            navigation.navigate("Game", { gameId: '777' })
+          }} />
+      </ScrollView>
+    </GradientBackground>
   )
 }
