cf052d24f9de71b07f1821a929a6be34297e2417
Keagan St. Rose <sanvicented1@gmail.com>
2022-09-27 15:25:38 -0400

    react-navigation, navigates between home and game screens

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,21 @@
 import React, { ReactElement } from 'react'
 import { StyleSheet, View } from 'react-native';
 import { Text, AppBootstrap } from "./components/"
+import Navigator from "./config/navigator"
+import 'react-native-gesture-handler';
 
 export default function App(): ReactElement {
   return (
     <AppBootstrap>
-      <View style={styles.container}>
+      <Navigator />
+      {/* <View style={styles.container}>
         <Text
           onPress={() => { alert(true) }}
           style={{ fontSize: 40 }}
         >
-          Hello world!!
+          Hello world!!!
         </Text>
-      </View>
+      </View> */}
     </AppBootstrap>
   );
 }
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,12 +1,23 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
+import { View, Text, Image, Button } from 'react-native'
+import React, { ReactElement } from 'react'
 import styles from "./home.styles"
+import { StackNavigationProp } from "@react-navigation/stack"
+import { StackNavigatorParams } from "../../config/navigator"
 
-export default function Home() {
+type HomeProps = {
+  navigation: StackNavigationProp<StackNavigatorParams, "Home">
+}
+
+export default function Home({ navigation }: HomeProps): ReactElement {
   return (
     <View style={styles.container}>
-       <Image source={require("../../../assets/favicon.png")}/>
+      <Image source={require("../../../assets/favicon.png")} />
       <Text>Home</Text>
+      <Button
+        title="Game"
+        onPress={() => {
+          navigation.navigate("Game", { gameId: '777' })
+        }} />
     </View>
   )
 }
