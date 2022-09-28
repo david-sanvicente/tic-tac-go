cfb107a7578320ab81f29db3c11d9c4a2aeb6820
Keagan St. Rose <sanvicented1@gmail.com>
2022-09-28 14:45:33 -0400

    logic for game board, determine winner or draw

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -4,15 +4,15 @@ import styles from "./single-player-game.styles"
 import { GradientBackground } from "../../components"
 import { Board } from "../../components"
 import Text from "../../components/text/text"
-import { printFormattedBoard, isEmpty, isFull, getAvailableMoves, BoardState } from '../../utils'
+import { printFormattedBoard, isEmpty, isFull, getAvailableMoves, isTerminal, BoardState } from '../../utils'
 // import { printFormattedBoard } from "../../utils"
 
 export default function Game(): ReactElement {
-  const b: BoardState = ['o', 'o', null, 'x', 'x', null, 'x', 'o', null]
+  const b: BoardState = [null, 'o', null, 'x', 'o', null, 'x', 'o', null]
   printFormattedBoard(b);
-  console.log(isEmpty(b))
-  console.log(isFull(b))
-  console.log(getAvailableMoves(b))
+  console.log(isTerminal(b))
+  // console.log(isFull(b))
+  // console.log(getAvailableMoves(b))
   return (
     <GradientBackground>
       <SafeAreaView style={styles.container}>
--- src/utils/board.ts
+++ src/utils/board.ts
@@ -1,4 +1,4 @@
-import { BoardState } from '../utils'
+import { BoardState, BoardResult } from '../utils'
 import {Moves} from '../utils/types'
 
 export const printFormattedBoard = (state: BoardState): void => {
@@ -33,4 +33,39 @@ export const getAvailableMoves = (state: BoardState): Moves[] => {
   })
 
   return moves
+}
+
+export const isTerminal = (state: BoardState): BoardResult | false => {
+  if(isEmpty(state)) return false
+  const winningLines = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+  for(let i = 0; i < winningLines.length; i++){
+    const line = winningLines[i]
+    const [cell1, cell2, cell3] = line
+    if(state[cell1] && state[cell1] === state[cell2] && state[cell1] === state[cell3]){
+      const result: BoardResult = {
+        winner: state[cell1]
+      }
+      if(i < 3){
+        result.direction = "H"
+        result.row = i === 0 ? 1 : i === 1 ? 2 : 3
+      }
+      if(i >= 3 && i <= 5){
+        result.direction = "V"
+        result.column = i === 3 ? 1 : i === 4 ? 2 : 3
+      }
+      if(i > 5){
+        result.direction = "D"
+        result.diagonal = i === 6 ? "MAIN" : "COUNTER"
+      }
+
+      return result
+    }
+  }
+  if(isFull(state)){
+    return {
+      winner: null
+    }
+  }
+  return false
 }
