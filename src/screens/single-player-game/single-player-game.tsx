7b9d7171236a68899a36cff7249cd06b2b6634a0
Keagan St. Rose <sanvicented1@gmail.com>
2022-09-28 13:36:22 -0400

    printFormattedBoard util function, typescript for BoardState

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -4,14 +4,18 @@ import styles from "./single-player-game.styles"
 import { GradientBackground } from "../../components"
 import { Board } from "../../components"
 import Text from "../../components/text/text"
+import { printFormattedBoard, BoardState } from '../../utils'
+// import { printFormattedBoard } from "../../utils"
 
 export default function Game(): ReactElement {
+  const b: BoardState = ['o', 'o', null, 'x', 'x', null, 'x', 'o', null]
+  printFormattedBoard(b);
   return (
     <GradientBackground>
       <SafeAreaView style={styles.container}>
         <Board
           onCellPressed={(idx) => alert(idx)}
-          state={['x', 'o', null, 'x', 'o', null, 'x', 'o', null]}
+          state={b}
           size={300}
         />
       </SafeAreaView>
